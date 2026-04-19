634189f5ae819cba6bf341996a617502041c4119
박범규 <bumkyu84259392@c5r8s4.codyssey.kr>
2026-04-19 20:36:19 +0900

    feature/Quiz.__init__ 작성

--- Quiz.py
+++ Quiz.py
@@ -2,7 +2,9 @@
 
 class Quiz:
     def __init__(self, questions):
-        pass
+        self.questions = questions
+        self.score = 0
+        self.question_number = 0
 
     def run_quiz(self):
         pass
